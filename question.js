b16d735d0326e2668da0962fb058f533fd751a61
Engin Karataş <enginkaratas99@gmail.com>
2021-01-03 21:33:14 +0300

    working with little issues

--- question.js
+++ question.js
@@ -14,24 +14,26 @@ class Question {
             squareColor = color(100 + sin(millis() / 800) * 100, 100, 59);
             squareColor.setAlpha(128 + 44 * sin(millis() / 800));
             fill(squareColor);
-            ellipse(this.locations[0][a] + 25, this.locations[1][a] - 8, 100, 100)
+            ellipse(this.locations[0][a] + 40, this.locations[1][a] - 8, 200, 100)
             fill(0)
             textSize(25)
             text(this.baloons[a], this.locations[0][a], this.locations[1][a])
             textSize(30)
             text(this.questionLine, 400, windowHeight * 0.7)
+            text("score"+scorea,530,35)
         }
     }
     update() {
 
-        let ang1 = radians(angle1) * 0.4;//velocty
+        let ang1 = radians(angle1) * 0.2;//velocty
         let y1 = height / 2 + scalar * sin(ang1);
         let y2 = height / 2 + -scalar * sin(ang1);
         fill(255, 204, 0);
         angle1 += 2;
-        // this.locations[1][0] = y1
-        // this.locations[1][1] = y2
-        // this.locations[1][2] = y1
+        
+        this.locations[1][0] = y1
+        this.locations[1][1] = y2
+        this.locations[1][2] = y1
          
     }
     clicked() {
@@ -42,10 +44,9 @@ class Question {
             if (d < this.r) { 
                 if (this.trueBaloons[questionCounter][y]==1) {
                     questionCounter++;
-                    score++;
+                    scorea++;
                 }
-                else
-                    score--;
+             
 
             }
         }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-let score = 0;
+let scorea = 0;
 let squareColor;
 let questions = [];
 let baloons = [];
-let baloonPool =[];
-        
+let baloonPool = [];
+
 let angle1 = 0;
-let scalar = 200;//baloons directon and size
+let scalar = 100;//baloons directon and size
 
 let questionCounter = 0;
 
@@ -13,22 +13,44 @@ let numberOfQuestion = 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  baloonPool = [["he/is","she/it","they"],["xx","yy","zz","dd"],["he/is","she/it","they"],["xx","yy","zz","dd"]]
-  trueBaloons = [[0,1,0],[0,0,1],[0,1,0],[0,0,1]]
-  questionPool = [["What time ___ ?"],["this __ question 2"],["What time ___ ?"],["this __ question 2"]]
-  locationPool = [[250,550,850],[100,100,100],[250,550,850],[100,100,100]]
+
   
+  questionPool = [
+    ["Jul: Did ___ see me ? Dan: No I ___"],
+    ["Dan: What is __ name ? Jul: ___ name is Merve"],
+    ["What time ___ ?"],
+    ["Are you able __ call me ?"]]
+
+  baloonPool = [
+    ["it/do","do/it","you/didn`t"],
+    ["like/our", "your/my", "do/was"],
+    ["is it", "she/it", "they"],
+    ["can`t", "do", "to"]]
+
+
+  trueBaloons = [
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0],
+    [0, 0, 1]]
+
+  locationPool = [
+    [250, 550, 850],
+    [100, 100, 100],
+    [250, 550, 850],
+    [100, 100, 100]]
+
   //QUESTİONS INITALİZE
-  for (let i = 0; i < questionPool.length;i++) {
-    questions.push(new Question(questionPool[i],baloonPool[i],locationPool,baloonPool[i].length,trueBaloons));
+  for (let i = 0; i < questionPool.length; i++) {
+    questions.push(new Question(questionPool[i], baloonPool[i], locationPool, baloonPool[i].length, trueBaloons));
   }
 
 }
 
 function draw() {
-  background(200)
+  background(200, 255, 255, 50)
 
-  
+
 
   for (let a = 0; a < questions.length; a++) {
     questions[questionCounter].show();
@@ -36,4 +58,3 @@ function draw() {
   }
 }
 
- 
