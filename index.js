8394a823d40ebc556a14d904d840056978c74188
Engin Karataş <enginkaratas99@gmail.com>
2021-01-24 21:19:58 +0300

    cluster, database, collection
    connected to mongodb. term learned: cluster, database, collection, document

--- index.js
+++ index.js
@@ -4,23 +4,36 @@ const express = require('express');
 const app = express(); //listining right now
 app.listen(3000, () => console.log("listening at 3000")); //3000 de dinleyeceğim
 app.use(express.static('public'));//kullanıcı tarafından erişilebilecek dosya 'public'dir.
-app.use(express.json({limit:"1mb"}));//server allows json and taken data size max 1mb, If this row not exist it will be undifined for request parameter 
-//---2.3--- post data and recive data 
+app.use(express.json({ limit: "1mb" }));//server allows json and taken data size max 1mb, If this row not exist it will be undifined for request parameter
+//---2.3--- post data and recive data
 // app.get('/index.html', (req, res) => {
 //   res.send('Hello World!')
 // })
 
-var Datastore = require('nedb'), db = new Datastore({filename : 'database'});
-db.loadDatabase();
+//db
+const connectionString = 'mongodb+srv://sonaovski:Exo-craft01@cluster0.141km.mongodb.net/revision?retryWrites=true&w=majority';
+const MongoClient = require('mongodb').MongoClient
+//**************************************************************** */
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
 
-app.post('/api', (request, response) => {
-  console.log("request geldi");
-  db.insert(request.body);
-});
 
-app.get('/sa', (req, res) => {
-  db.find({questionPool:"asda"}, function (err,docs){ 
-    res.send('H e l l o - W o r l d !'+docs.questionPool)
+ 
+    quotesCollection.insertOne({"hello":222});
 
+  app.post('/addquestion', (request, response) => {
+    console.log("request geldi");
+    quotesCollection.insertOne()
   });
+  
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html')
+  })
+  
 })
+//**************************************************************** */
+
+ 
