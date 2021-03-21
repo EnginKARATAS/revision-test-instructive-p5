0ff47f6f8d89b88fb767b6638a60ef27f8142254
Engin Karataş <enginkaratas99@gmail.com>
2021-03-21 20:37:25 +0300

    index.js updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.listen(3000, () => console.log("listening at 3000")); //3000 de dinleyeceği
 app.use(express.static('public'));//kullanıcı tarafından erişilebilecek dosya 'public'dir.
 app.use(express.json({ limit: "1mb" }));//server allows json and taken data size max 1mb, If this row not exist it will be undifined for request parameter
 
- 
+
 
 //***DB*********************************************************** */
 const connectionString = 'mongodb+srv://sonaovski:Exo-craft01@cluster0.141km.mongodb.net/revision?retryWrites=true&w=majority';
@@ -22,27 +22,28 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 
     app.post('/api', (req, res) => {
       console.log("api geldi");
-      quotesCollection.insertOne(req.body).catch(err => {console.log(err);})
+      quotesCollection.insertOne(req.body)
+      .catch(err => { console.log(err); })
     })
 
-    app.get('/products',(req,res)=>{
+    app.get('/products', (req, res) => {
       db.collection('quotes').find().toArray()
-      .then(result => {
-        res.send(result);
-      })
-    })   
+        .then(result => {
+          res.send(result);
+        })
+    })
 
-     app.get('/', (req, res) => {
-      res.sendFile(__dirname +'\\public' +'\\indexs.html')
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '\\public' + '\\indexs.html')
 
-   
-      .catch(error => {console.log(error)});
+
+        .catch(error => { console.log(error) });
       // res.sendFile(__dirname + 'index.html')
     })
 
   })
-  
- 
+
+
 //**************************************************************** */
 
 
